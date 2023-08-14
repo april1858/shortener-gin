a643976124f7b029e1a9c788522f0213167a2cad
april1858 <april1858@april1858.april1858>
2023-08-14 20:53:17 +0700

    iter555

--- internal/pkg/app/app.go
+++ internal/pkg/app/app.go
@@ -40,7 +40,7 @@ func New() (*App, error) {
 
 func (a *App) Run() error {
 	fmt.Println("server running. PORT :" + a.c.ServerAddress)
-	a.rr.Run(a.c.ServerAddress)
+	a.rr.Run(":" + a.c.ServerAddress)
 
 	return nil
 }
