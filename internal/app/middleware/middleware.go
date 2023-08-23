f04b02116d46c8dffbb2bcc34277db67f3c718aa
april1858 <april1858@april1858.april1858>
2023-08-23 18:10:04 +0700

    at

--- internal/app/middleware/middleware.go
+++ internal/app/middleware/middleware.go
@@ -1,8 +1,6 @@
 package middleware
 
 import (
-	"fmt"
-
 	"github.com/gin-contrib/gzip"
 	"github.com/gin-gonic/gin"
 )
@@ -16,20 +14,12 @@ func New() *MW {
 
 func (mv *MW) GZIP() gin.HandlerFunc {
 	return func(c *gin.Context) {
-
 		if c.GetHeader("Accept-Encoding") == "gzip" {
-			fmt.Println("c1.Request - ", c.Request)
-			fmt.Println("c1.Params - ", c.Params)
 			if c.GetHeader("Content-Encoding") == "gzip" {
-				fmt.Println("c2.Request - ", c.Request)
-				fmt.Println("c2.Params - ", c.Params)
 				gzip.DefaultDecompressHandle(c)
 			}
 			gzip.Gzip(gzip.DefaultCompression)
-			fmt.Println("c3.Request - ", c.Request)
-			fmt.Println("c3.Params - ", c.Params)
 		}
-
 		c.Next()
 	}
 }
