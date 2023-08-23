f985c17feb6471eae43cf71c3ccc6d5200eb35f6
april1858 <april1858@april1858.april1858>
2023-08-23 17:55:16 +0700

    at

--- internal/app/middleware/middleware.go
+++ internal/app/middleware/middleware.go
@@ -20,12 +20,14 @@ func (mv *MW) GZIP() gin.HandlerFunc {
 		if c.GetHeader("Accept-Encoding") == "gzip" {
 			fmt.Println("c1.Request - ", c.Request)
 			fmt.Println("c1.Params - ", c.Params)
-			if c.GetHeader("Contenet-Encoding") == "gzip" {
+			if c.GetHeader("Content-Encoding") == "gzip" {
 				fmt.Println("c2.Request - ", c.Request)
 				fmt.Println("c2.Params - ", c.Params)
 				gzip.DefaultDecompressHandle(c)
 			}
 			gzip.Gzip(gzip.DefaultCompression)
+			fmt.Println("c3.Request - ", c.Request)
+			fmt.Println("c3.Params - ", c.Params)
 		}
 
 		c.Next()
