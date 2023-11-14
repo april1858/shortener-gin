a5e26b76c7d07f3eb548bca77881659a280ef883
april1858 <april1858@april1858.april1858>
2023-11-14 14:45:51 +0700

    first66

--- internal/app/repository/postgres.go
+++ internal/app/repository/postgres.go
@@ -137,7 +137,7 @@ func (d *DB) Del(p []S) {
 				fmt.Println("err postgres -", err)
 			}
 		}
-		time.Sleep(29 * time.Second)
+		time.Sleep(69 * time.Second)
 		_, err := db.Exec(ctx, `DELETE FROM shortener6 WHERE condition = false`)
 
 		if err != nil {
